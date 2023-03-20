035bfcfa514f6017c4150270ece17d17e45667fe
Егор Танин <sealynxxxx@gmail.com>
2023-03-20 11:26:54 +0300

    Init

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include "dialog.h"
+#include <QApplication>
+
+int main(int argc, char **argv)
+{
+    QApplication app(argc, argv);
+    Dialog *dialog = new Dialog;
+    dialog->show();
+
+    return app.exec();
+}
