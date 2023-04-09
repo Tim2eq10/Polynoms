b343971e6b23117be9c1902951123b8b3ba7048c
Egor Tanin <sealynxxxx@gmail.com>
2023-04-09 18:17:39 +0300

    first step implementation of ui logic

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -45,5 +45,34 @@ void MainWindow::on_pushButton_2_clicked()
 
 void MainWindow::on_pushButton_clicked()
 {
-    ui->tableWidget->insertRow( ui->tableWidget->rowCount() );
+    // TODO
+        // 1. Parse lineEdit.text()
+        // 2. Check if we have this name in tables
+        //   3.a Update val in tables and QTableWidget
+        //   3.b Set new <key, value> in tables and add new row in QTableWidget
+
+        QString data = ui->lineEdit->text();
+
+        QString polynomName = static_cast<QString>(data[0]);
+        QString polynomData = "";
+
+        for (size_t i = 2; i < static_cast<size_t>(data.size()); i++)
+            if (data[i] != ' ' && data[i] != '=')
+                polynomData += data[i];
+
+        Polynom polynom(polynomData.toStdString());
+
+        if (allTables.find(polynomName.toStdString()) == false) {
+            allTables.insert(polynomName.toStdString(), polynom);
+
+            ui->tableWidget->insertRow(ui->tableWidget->rowCount());
+            QTableWidgetItem *itemName = new QTableWidgetItem(polynomName);
+            QTableWidgetItem *itemPolynom = new QTableWidgetItem(polynomData);
+
+            ui->tableWidget->setItem(ui->tableWidget->rowCount() - 1, 0, itemName);
+            ui->tableWidget->setItem(ui->tableWidget->rowCount() - 1, 1, itemPolynom);
+        }
+        else {
+
+        }
 }
